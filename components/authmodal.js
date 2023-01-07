133a72444de5a2c3196fef9199d2cb97f2f3ff01
garvit <agarvit1142000@gmail.com>
2023-01-08 00:41:47 +0530

    authmodal popup functionality

--- components/authmodal.js
+++ components/authmodal.js
@@ -1,16 +1,94 @@
+import { useState } from "react";
+
 export default function AuthModal() {
+  useState[(Name, setName)] = useState("");
   return (
     <div
       style={{
-        position: "fixed",
+        height: "100vh",
+        width: "100vw",
         display: "flex",
-        justifyContent: "center",
-        backgroundColor: "green",
         position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
+        top: "0",
+        left: "0",
+        zIndex: "1",
+        backgroundColor: "rgba(0,0,0,0.5)",
       }}
-    ></div>
+    >
+      <div
+        style={{
+          opacity: "1",
+          filter: "blur(0px)",
+          borderStyle: "solid",
+          borderColor: "black",
+          borderWidth: "2px",
+          borderRadius: "12px",
+
+          backgroundColor: "#F5F5F5",
+          height: "531px",
+          width: "473px",
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+
+            flexDirection: "row",
+
+            height: "64px ",
+          }}
+        >
+          <button
+            style={{
+              borderLeft: "1px solid #000000",
+              fontSize: "24px",
+              borderRadius: "12px 0px 0px 0px",
+              width: "50%",
+              fontWeight: "400",
+            }}
+          >
+            Login
+          </button>
+          <button
+            style={{
+              borderLeft: "1px solid #000000",
+              fontSize: "24px",
+              width: "50%",
+              borderRadius: " 0px 12px 0px 0px ",
+              fontWeight: "400",
+            }}
+          >
+            Sign-Up
+          </button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundColor: "green",
+          }}
+        >
+          <div>
+            <input
+              style={{
+                width: "382px",
+                height: "53px",
+                border: "1px solid #000000",
+                borderRadius: "12px",
+              }}
+              type={"text"}
+            ></input>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -21,63 +21,65 @@ if (!firebase.apps.length) {
 const db = firebase.firestore();
 export default function Home() {
   const [open, setOpen] = useState(false);
+
   return (
     <div
       style={{
         display: "flex",
         height: "100vh",
-        width: "100vh",
-        justifyContent: "space-around",
-
+        width: "100vw",
+        justifyContent: "space-evenly",
+        // filter: open ? "blur(2px)" : "none",
         padding: "0",
         margin: "0",
       }}
     >
+      {open && <AuthModal />}
       <div
         style={{
           display: "flex",
           flexDirection: "column",
+
           margin: "0",
-          width: "50vh",
+          width: "50vw",
           padding: "0",
-          marginRight: "200px",
+
           justifyContent: "center",
-          backgroundColor: "pink",
         }}
       >
-        <div style={{ fontSize: "56px" }}>Connect with your </div>
-        <div style={{ fontSize: "56px" }}>Peers</div>
-        <div>
-          <button
-            style={{
-              color: "white",
-              backgroundColor: "#24a0ed",
-              height: "58px",
-              Width: "500px",
-              fontSize: "32px",
-              borderRadius: "12px",
-              border: "none",
-              padding: "0 35px",
-              margin: "40px 0",
-            }}
-            onClick={() => setOpen(true)}
-          >
-            Continue
-          </button>
+        <div style={{ paddingLeft: "100px" }}>
+          <div style={{ fontSize: "56px" }}>Connect with your </div>
+          <div style={{ fontSize: "56px" }}>Peers</div>
+          <div>
+            <button
+              style={{
+                color: "white",
+                backgroundColor: "#24a0ed",
+                height: "58px",
+                Width: "500px",
+                fontSize: "32px",
+                borderRadius: "12px",
+                border: "none",
+                padding: "0 35px",
+                margin: "40px 0",
+              }}
+              onClick={() => setOpen(true)}
+            >
+              Continue
+            </button>
+          </div>
         </div>
       </div>
       <div
         style={{
-          backgroundColor: "Green",
           display: "flex",
           alignItems: "center",
+          width: "50vw",
+          backgroundSize: "cover",
+          backgroundImage:
+            "url(https://www.loginvsi.com/wp-content/uploads/2022/06/shutterstock_2150205609-overview_3-e1655135806812.png)",
         }}
-      >
-        <img
-          style={{ width: "677px", height: "832px" }}
-          src="https://www.loginvsi.com/wp-content/uploads/2022/06/shutterstock_2150205609-overview_3-e1655135806812.png"
-        />
-      </div>
+      ></div>
     </div>
   );
 }
