07760d3975e2820b679878c57e2a3402126e7195
Rakshit Arora <rakshit087@gmail.com>
2023-03-04 00:51:54 +0530

    fixed login and signup margins

--- components/login.js
+++ components/login.js
@@ -30,18 +30,17 @@ export default function Login() {
           paddingLeft: "15px",
         }}
         type={"text"}
-        placeholder={"UserName"}
+        placeholder={"Username"}
         onChange={(e) => setName(e.target.value)}
-      ></input>
-
+      />
       <input
         style={{
           width: "382px",
           height: "53px",
           border: "1px solid #000000",
           borderRadius: "12px",
-          marginTop: "78px",
           color: "#626262",
+          marginTop: "21px",
           fontSize: "18px",
           lineHeight: "27px",
           fontWeight: "400",
@@ -92,39 +91,6 @@ export default function Login() {
         }}
       >
         Login
-      </button>
-      <div
-        style={{
-          width: "19px",
-          height: "21px",
-          color: "#000000",
-          marginTop: "15px",
-          fontWeight: "400",
-          fontSize: "18px",
-          lineHeight: "27px",
-        }}
-      >
-        or
-      </div>
-
-      <button
-        style={{
-          height: "46px",
-          width: "382px",
-          marginTop: "14px",
-          backgroundColor: "#FFFFFF",
-          borderRadius: "12px",
-          paddingLeft: "15px",
-          border: "1px solid #000000",
-          lineHeight: "27px",
-          fontSize: "17px",
-          fontWeight: "400",
-          color: "#626262",
-          textAlign: "center",
-        }}
-      >
-        <FontAwesomeIcon icon="coffee" />
-        Continue with Google
       </button>
     </div>
   );
--- components/signup.js
+++ components/signup.js
@@ -21,17 +21,17 @@ export default function SignUp() {
           height: "53px",
           border: "1px solid #000000",
           borderRadius: "12px",
-          marginTop: "60px",
           color: "#626262",
           fontSize: "18px",
           lineHeight: "27px",
           fontWeight: "400",
           paddingLeft: "15px",
+          marginTop: "78px",
         }}
         type={"text"}
-        placeholder={"Your Name"}
+        placeholder={"Username"}
         onChange={(e) => setName(e.target.value)}
-      ></input>
+      />
 
       <input
         style={{
@@ -72,7 +72,7 @@ export default function SignUp() {
         style={{
           height: "46px",
           width: "382px",
-          marginTop: "34px",
+          marginTop: "61px",
           backgroundColor: "#2757EA",
           borderRadius: "12px",
           paddingLeft: "15px",
@@ -96,39 +96,6 @@ export default function SignUp() {
         }}
       >
         Sign Me Up !
-      </button>
-      <div
-        style={{
-          width: "19px",
-          height: "21px",
-          color: "#000000",
-          marginTop: "15px",
-          fontWeight: "400",
-          fontSize: "18px",
-          lineHeight: "27px",
-        }}
-      >
-        or
-      </div>
-
-      <button
-        style={{
-          height: "46px",
-          width: "382px",
-          marginTop: "14px",
-          backgroundColor: "#FFFFFF",
-          borderRadius: "12px",
-          paddingLeft: "15px",
-          border: "1px solid #000000",
-          lineHeight: "27px",
-          fontSize: "17px",
-          fontWeight: "400",
-          color: "#626262",
-          textAlign: "center",
-        }}
-      >
-        <FontAwesomeIcon icon="coffee" />
-        Continue with Google
       </button>
     </div>
   );
