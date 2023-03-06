05cf4d70ce253197333019fb69a2938d16b998df
Rakshit Arora <rakshit087@gmail.com>
2023-03-07 00:57:20 +0530

    redirect from dashboard if not logged in

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,14 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context/auth";
+
 export const Dashboard = () => {
+  const { loggedIn, setLoggedIn } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!loggedIn) {
+      router.push("/");
+    }
+  }, [loggedIn]);
+
   return <div>Hello World</div>;
 };
