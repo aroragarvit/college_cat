ec3c08e27358c08a5c9f7e93900967983f69e171
Rakshit Arora <rakshit087@gmail.com>
2023-03-07 00:57:59 +0530

    initial login state to false

--- context/auth.js
+++ context/auth.js
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(true);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <AuthContext.Provider
